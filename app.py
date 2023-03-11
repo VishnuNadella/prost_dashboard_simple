7efb193e11deed85a7b1659e19a80435b4355419
VishnuNadella <63490737+VishnuNadella@users.noreply.github.com>
2023-03-11 18:51:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -42,8 +42,7 @@ st.header(remin_cnt_str)
 
 phone_no = st.text_input("Enter Phone number to search:")
 req_det = collection.find_one({"phone_no": phone_no})
-st.write(phone_no)
-st.write(req_det)
+
 if req_det is not None and phone_no != "":
     st.write(req_det)
 elif req_det is None and phone_no != "":
